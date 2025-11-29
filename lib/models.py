a5c93dc943299fc82b8f2ec4d6cf06b56b0bdf2d
Susan Gakii <susangakii71@gmail.com>
2025-11-29 11:13:25 +0300

    translates orms to sqlalchemy

--- lib/models.py
+++ lib/models.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from sqlalchemy import (Column, String, Integer)
+from sqlalchemy import (Column, String, Integer, create_engine)
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
